e672d1e90b19396710a99b8c1ee297a6fca45f52
rjxff614-web <rjxff614@gmail.com>
2026-04-11 13:11:01 +0530

    Update __main__.py

--- Elevenyts/__main__.py
+++ Elevenyts/__main__.py
@@ -125,3 +125,16 @@ if __name__ == "__main__":
                 loop.stop()
         except:
             pass
+            from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running ✅"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+Thread(target=run).start()
