b86b6cfccb2e03d89f57e55ab1972b594f590dd9
rjxff614-web <rjxff614@gmail.com>
2026-04-11 23:16:56 +0530

    Create Tagall.py

---
+++ Elevenyts/plugins/Tagall.py
@@ -0,0 +1,22 @@
+from bot import app
+from pyrogram import filters
+
+@app.on_message(filters.command(["tagall", "all"]) & filters.group)
+async def tagall(client, message):
+
+    members = []
+
+    async for m in client.get_chat_members(message.chat.id):
+        if m.user and not m.user.is_bot:
+            members.append(m.user.mention)
+
+        if len(members) >= 50:
+            break
+
+    if not members:
+        return await message.reply_text("❌ No members found")
+
+    text = "🔥 TAG ALL 🔥\n\n"
+    text += " ".join(members)
+
+    await message.reply_text(text)
